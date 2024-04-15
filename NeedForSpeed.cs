395655dcf51b8b5acd165c19d00e51c4d95ba26c
KevinYakha <kevin@yakha.eu>
2024-04-15 10:46:36 +0200

    added constructors to the RemoteControlCar and RaceTrack classes

--- NeedForSpeed.cs
+++ NeedForSpeed.cs
@@ -3,7 +3,12 @@ using System;
 class RemoteControlCar
 {
     // TODO: define the constructor for the 'RemoteControlCar' class
-
+    public RemoteControlCar(int speed, int batteryDrain)
+    {
+        _speed = speed;
+        _batteryDrain = batteryDrain;
+    }
+    
     public bool BatteryDrained()
     {
         throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDrained() method");
@@ -23,14 +28,23 @@ class RemoteControlCar
     {
         throw new NotImplementedException("Please implement the (static) RemoteControlCar.Nitro() method");
     }
+
+    private int _speed;
+    private int _batteryDrain;
 }
 
 class RaceTrack
 {
     // TODO: define the constructor for the 'RaceTrack' class
+    public RaceTrack(int distance)
+    {
+        _distance = distance;
+    }
 
     public bool TryFinishTrack(RemoteControlCar car)
     {
         throw new NotImplementedException("Please implement the RaceTrack.TryFinishTrack() method");
     }
+
+    private int _distance;
 }
