3aa77403737fee3f97f48860729928c890a9201f
KevinYakha <kevin@yakha.eu>
2024-04-15 10:58:25 +0200

    added Nitro

--- NeedForSpeed.cs
+++ NeedForSpeed.cs
@@ -30,7 +30,7 @@ class RemoteControlCar
 
     public static RemoteControlCar Nitro()
     {
-        throw new NotImplementedException("Please implement the (static) RemoteControlCar.Nitro() method");
+        return new RemoteControlCar(50, 4);
     }
 
     private int _speed = 0;
